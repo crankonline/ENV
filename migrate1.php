3c44e25fb0fd2a9ceaa375cc0a7cefc63d1ba981
dex <evilz@bk.ru>
2019-07-23 15:40:02 +0600

    migrate 23.07.19

---
+++ migrate1.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dex
+ * Date: 23.07.19
+ * Time: 14:10
+ */
+require 'core/configuration.php';
+
+use Unikum\Core\Dbms\ConnectionManager as Connections;
+
+
+class migrate1 {
+
+
+	public function insertModule( ) {
+		$sql = <<<SQL
+INSERT INTO "Core"."Module"
+    ("IDModule", "ModuleGroupID", "AccessKey", "HandlerClass", "Name", "IsEntryPoint")
+VALUES
+    (
+        DEFAULT,
+        8,
+        'reregister',
+        'Reregister',
+        'Перерегистрация клиентов',
+        TRUE
+    )RETURNING
+    "IDModule";;
+
+
+SQL;
+
+		$stmt = Connections::getConnection( 'Environment' )->prepare( $sql );
+
+		$stmt->execute( [
+////			'id' => 42,
+//			'moduleGroupId' => 8,
+//			'accesKey' => 'reregister',
+//			'handleClass' => 'Reregister',
+//			'name' => 'Перерегистрация клиентов',
+//			'isEntryPoint' => true
+		] );
+
+		return $stmt->fetchColumn();
+	}
+
+	public function insertModuleAccess( $id ) {
+		$sql = <<<SQL
+INSERT INTO "Core"."ModulePermission"
+    ("IDModulePermission", "ModuleID", "Mark", "Name")
+VALUES
+    (
+        DEFAULT,
+        :id,
+        'can-access',
+        'Доступ к модулю'
+    );
+
+
+SQL;
+
+		$stmt = Connections::getConnection( 'Environment' )->prepare( $sql );
+
+		$stmt->execute( [
+			'id' => $id,
+		] );
+
+		return $stmt->fetchColumn();
+	}
+}
+
+$migrate = new migrate1();
+
+$t = $migrate->insertModule();
+$migrate->insertModuleAccess($t);
+
+print_r($t);
+
+
+
