708d04450b9d139dad23702716302580ee3aa501
Vitalii <v.kostash@dostek.kg>
2020-09-11 09:29:29 +0600

    ENV-58 (Правки по замечаниям)

---
+++ core/modules/PayFilter/AccountAndDateAndType.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace Environment\Modules\PayFilter;
+
+
+class AccountAndDateAndType extends \Environment\Modules\PayFilter\PayFilter
+{
+    public function setParams($values) {
+       $this->params = '"p"."Account" LIKE :f_account AND ("p"."DateTime" BETWEEN :f_d_min AND :f_d_max) AND "p"."Type" = :f_type';
+        $this->values = ([
+            'f_account' => '%'.$values['account'].'%',
+            'f_d_min'  => $values['dateMin'],
+            'f_d_max'  => $this->getDateMax($values['dateMax']),
+            'f_type'  => $values['type']
+        ]);
+    }
+
+}
---
+++ core/modules/PayFilter/DateAndType.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace Environment\Modules\PayFilter;
+
+
+class DateAndType extends \Environment\Modules\PayFilter\PayFilter
+{
+    public function setParams($values) {
+        $this->params = '"p"."Type" = :f_type AND ("p"."DateTime" BETWEEN :f_d_min AND :f_d_max)';
+        $this->values = ([
+            'f_d_min'  => $values['dateMin'],
+            'f_d_max'  => $this->getDateMax($values['dateMax']),
+            'f_type'   => $values['type']
+        ]);
+    }
+
+}
